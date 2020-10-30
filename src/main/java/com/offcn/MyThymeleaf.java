cd4199bcef80aefb41db118d586299bee16ca8a6
lyh <1426024098@qq.com>
2020-10-30 13:37:46 +0800

    idea的第二次提交

---
+++ src/main/java/com/offcn/MyThymeleaf.java
@@ -0,0 +1,13 @@
+package com.offcn;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyThymeleaf {
+
+    public String demo(){
+        return "mydemo";
+    }
+
+}
